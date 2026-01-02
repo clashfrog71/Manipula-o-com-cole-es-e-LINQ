b396279d2a688e1e3125615f6dc19beb5c5742fb
Tiago Pereira <clashfrog71@gmail.com>
2026-01-01 23:54:42 -0300

    fazando outro uso so yield

--- Manipulação com coleções e LINQ/Program.cs
+++ Manipulação com coleções e LINQ/Program.cs
@@ -13,9 +13,23 @@ IEnumerable<int> numerosPares2(int limite)
     for (int i = 1; i <= limite; i += 2)
     {
        yield return i;
+
     }
 }
 foreach (var numero in pares)
 {
     Console.WriteLine(numero);
+}
+static IEnumerable<string> GerarRelatorio()
+{
+    yield return "Início do relatório";
+    yield return "Seção 1: Dados gerais";
+    yield return "Seção 2: Estatísticas";
+    yield return "Seção 3: Conclusões";
+    yield return "Fim do relatório";
+}
+
+foreach (var parte in GerarRelatorio())
+{
+    Console.WriteLine(parte);
 }
