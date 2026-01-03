715a255f29b52f9d6bfe1ed958afdc0f0dad8126
Tiago Pereira <clashfrog71@gmail.com>
2026-01-02 23:43:39 -0300

    manipulando arrays

--- Manipulação com coleções e LINQ/Program.cs
+++ Manipulação com coleções e LINQ/Program.cs
@@ -1,35 +1,20 @@
-﻿var pares = numerosPares(10);
-IEnumerable<int> numerosPares(int limite)
-{
-    var listaPares = new List<int>();
-    for (int i = 1; i <= limite; i+=2)
-    {
-       listaPares.Add(i);
-    }return listaPares;
-}
-IEnumerable<int> numerosPares2(int limite)
-{
-    var listaPares = new List<int>();
-    for (int i = 1; i <= limite; i += 2)
-    {
-       yield return i;
+﻿using System.Threading.Channels;
 
+class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        string[] diasDaSemana = new string[]
+     {
+        "Domingo",
+        "Segunda-feira",
+        "Terça-feira",
+        "Quarta-feira",
+        "Quinta-feira",
+        "Sexta-feira",
+        "Sábado"
+     };
+        Console.WriteLine(diasDaSemana[0]);
     }
-}
-foreach (var numero in pares)
-{
-    Console.WriteLine(numero);
-}
-static IEnumerable<string> GerarRelatorio()
-{
-    yield return "Início do relatório";
-    yield return "Seção 1: Dados gerais";
-    yield return "Seção 2: Estatísticas";
-    yield return "Seção 3: Conclusões";
-    yield return "Fim do relatório";
-}
-
-foreach (var parte in GerarRelatorio())
-{
-    Console.WriteLine(parte);
 }
