56b882d280461f1b0b4dd87dbe0b21e9d14436bb
Tiago Pereira <clashfrog71@gmail.com>
2026-01-12 23:57:30 -0300

    Adicionando metodos para editar listas e adicionar musicas

--- Manipulação com coleções e LINQ/Program.cs
+++ Manipulação com coleções e LINQ/Program.cs
@@ -1,11 +1,37 @@
-﻿public class Musica
+﻿using System.Collections;
+class Program
 {
-    public string Tiutulo { get; set; }
-    public string Artista { get; set; }
-    public int DuracaoEmSegundos { get; set; }
-}
-class playlist : IEnumerable<Musica>
-{
-    public void ie
+    public class Musica
+    {
+        public string Titulo { get; set; }
+        public string Artista { get; set; }
+        public int DuracaoEmSegundos { get; set; }
+    }
+    void exibirMusicas(Playlist playlist)
+    {
+        foreach (var musica in playlist)
+        {
+            Console.WriteLine($"Título: {musica.Titulo}, Artista: {musica.Artista}, Duração: {musica.DuracaoEmSegundos} segundos");
+        }
+    }
+    class Playlist : IEnumerable<Musica>
 
+    {
+        public string nome { get; set; }
+        private List<Musica> musicas = [];
+        public void AdicionarMusica(Musica musica)
+        {
+            musicas.Add(musica);
+        }
+
+        public IEnumerator<Musica> GetEnumerator()
+        {
+            return musicas.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
 }
