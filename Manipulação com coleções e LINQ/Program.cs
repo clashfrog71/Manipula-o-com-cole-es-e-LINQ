13cfedd1246a686696e2b5f13410795b15b84e21
Tiago Pereira <clashfrog71@gmail.com>
2026-01-16 00:29:24 -0300

    Adicionando metodo e utilizando metodo acharMusicasPeloTitulo

--- Manipulação com coleções e LINQ/Program.cs
+++ Manipulação com coleções e LINQ/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 class Program
 {
+    public static void Main(string[] Args)
+    {
+        Musica musica1 = new Musica { Titulo = "Imagine", Artista = "John Lemon", DuracaoEmSegundos = 183 };
+        Musica musica2 = new Musica { Titulo = "Bohemian Rhapsody", Artista = "Queen", DuracaoEmSegundos = 354 };
+        Musica musica3 = new Musica { Titulo = "Hotel California", Artista = "Eagles", DuracaoEmSegundos = 391 };
+        Playlist playlist = new Playlist { nome = "só jesus" };
+        playlist.AdicionarMusica(musica1);
+        playlist.AdicionarMusica(musica2);
+        playlist.AdicionarMusica(musica3);
+        playlist.acharMusicasPeloTitulo("Hotel");
+    }
     public class Musica
     {
         public string Titulo { get; set; }
         public string Artista { get; set; }
         public int DuracaoEmSegundos { get; set; }
     }
-    void exibirMusicas(Playlist playlist)
-    {
-        foreach (var musica in playlist)
-        {
-            Console.WriteLine($"Título: {musica.Titulo}, Artista: {musica.Artista}, Duração: {musica.DuracaoEmSegundos} segundos");
-        }
-    }
+    
     class Playlist : IEnumerable<Musica>
 
     {
@@ -29,13 +34,23 @@ class Program
         {
             return musicas.GetEnumerator();
         }
-        public void acharMusicas()
+        public void acharMusicasPeloTitulo(string titulo)
         {
-
+            musicas.Where(m => m.Titulo.Contains(titulo)).ToList().ForEach(m =>
+            {
+                Console.WriteLine($"Título: {m.Titulo}, Artista: {m.Artista}, Duração: {m.DuracaoEmSegundos} segundos");
+            });
         }
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+        internal void exibirMusicas(Playlist playlist)
+        {
+            foreach (var musica in playlist)
+            {
+                Console.WriteLine($"Título: {musica.Titulo}, Artista: {musica.Artista}, Duração: {musica.DuracaoEmSegundos} segundos");
+            }
         }
     }
 }
